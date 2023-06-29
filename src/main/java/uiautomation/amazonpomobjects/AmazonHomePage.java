b42e43219d09ebce864e47ce65d9a14fc8377c16
Tanishka <bist.tanishka@gmail.com>
2023-06-29 15:02:27 +0530

    Initialize commit

---
+++ src/main/java/uiautomation/amazonpomobjects/AmazonHomePage.java
@@ -0,0 +1,82 @@
+package uiautomation.amazonpomobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AmazonHomePage {
+
+	@FindBy(id = "nav-link-accountList-nav-line-1")
+	public
+	WebElement signin1;
+	
+	@FindBy(xpath = "//h1[normalize-space(text())=\"Sign in\"]")
+	public
+	WebElement signupdisplay;
+	
+	@FindBy(xpath = "//label[normalize-space(text())=\"Email or mobile phone number\"]")
+	public
+	WebElement emailphonedisplay;
+	
+	@FindBy(xpath = "//input[@type=\"email\"]")
+	public
+	WebElement emailinput;
+	
+	@FindBy(xpath = "//input[@type=\"submit\"]")
+	public
+	WebElement continuebutton;
+	
+	@FindBy(xpath = "//label[normalize-space(text())=\"Password\"]")
+	public
+	WebElement pwrddisplay;
+	
+	@FindBy(xpath = "//input[@type=\"password\"]")
+	public
+	WebElement enterpwd;
+	
+	@FindBy(xpath = "//input[@type=\"submit\"]")
+	public
+	WebElement signin;
+	
+	@FindBy(xpath = "//input[@id=\"twotabsearchtextbox\"]")
+	public
+	WebElement search;
+	
+	@FindBy(xpath = "//input[@id=\"nav-search-submit-button\"]")
+	public
+	WebElement selectpen;
+	
+	@FindBy(xpath = "//span[text()=\"Cello Bling Pastel Ball Pen | Blue Ball Pen | Jar of 25 Units | Best Ball Pens for Smooth Writing | Ball Point Pen Set | Pens for Students | Office Stationery Items | Best pen for Exam\"]")
+	public
+	WebElement hitpen1;
+	
+	//@FindBy(xpath ="//span[text()=\"Cello Signature Carbon Ball Pen | Blue Ball Pen | Pack of 1 | Premium Ball Pens | Best Ball Pen for Smooth Writing | Gifting Pens | Premium Pens Signature Pens\"]")
+	//public
+	//WebElement hitpen;
+	
+	@FindBy(xpath = "//input[@id=\"buy-now-button\"]")
+	public
+	WebElement buynow;
+	
+	//@FindBy(xpath = "//input[@id=\"pp-62IQFT-100\"]")
+	//public
+	//WebElement radiobutton;
+	
+	
+	@FindBy(xpath = "//input[@name=\"ppw-widgetEvent:SetPaymentPlanSelectContinueEvent\"]")
+	public
+	WebElement usethispaymentmethod;
+	
+	@FindBy(xpath = "//input[@name=\"placeYourOrder1\"]")
+	public
+	WebElement placeorder;
+	
+	public AmazonHomePage(WebDriver driver) {
+
+//initilize webelement by initelement method from pagefactory class
+	
+	PageFactory.initElements(driver, this);
+
+}
+}
---
+++ src/test/java/uiautomation/amazontestcases/Amazon.java
@@ -0,0 +1,83 @@
+package uiautomation.amazontestcases;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import uiautomation.amazonpomobjects.AmazonHomePage;
+
+public class Amazon {
+
+WebDriver driver;
+AmazonHomePage amazonhomepageobject;
+
+@BeforeTest
+public void launchbrowser() {
+WebDriverManager.chromedriver().setup();
+driver=new ChromeDriver();
+driver.get("https://www.amazon.in/");
+driver.manage().window().maximize();
+amazonhomepageobject=new AmazonHomePage(driver);
+}
+@Test
+public void signupaccount() {
+
+amazonhomepageobject.signin1.click();
+
+boolean singnupdi=amazonhomepageobject.signupdisplay.isDisplayed();
+Assert.assertEquals(singnupdi, true,"Signup not displayed");
+
+boolean emailphndisplay=amazonhomepageobject.emailphonedisplay.isDisplayed();
+Assert.assertEquals(emailphndisplay, true,"emailphndisplay not displayed");
+
+amazonhomepageobject.emailinput.sendKeys("8126921207");
+amazonhomepageobject.continuebutton.click();
+boolean passdisplay=amazonhomepageobject.pwrddisplay.isDisplayed();
+Assert.assertEquals(passdisplay, true,"Password not displayed");
+amazonhomepageobject.enterpwd.sendKeys("Karan@1234");
+amazonhomepageobject.signin.click();
+amazonhomepageobject.search.sendKeys("Pen under 150");
+amazonhomepageobject.selectpen.click();
+amazonhomepageobject.hitpen1.click();
+//we can use this as it will show window's id
+
+/*Set<String> s= driver.getWindowHandles();
+ArrayList ar=new ArrayList(s);
+System.out.println(ar.get(0));
+System.out.println(ar.get(1));
+driver.switchTo().window((String)ar.get(1));*/
+//or we can use below one also
+
+Set<String> newWindowHandles = driver.getWindowHandles();
+ArrayList<String> newHandlesList = new ArrayList<>(newWindowHandles);
+driver.switchTo().window(newHandlesList.get(1));
+amazonhomepageobject.buynow.click();
+
+Set<String> newWindowHandles1 = driver.getWindowHandles();
+ArrayList<String> newHandlesList1 = new ArrayList<>(newWindowHandles1);
+driver.switchTo().window(newHandlesList.get(1));
+amazonhomepageobject.usethispaymentmethod.click();
+
+placeorder();
+}
+
+public void placeorder() {
+   WebDriverWait wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.elementToBeClickable(amazonhomepageobject.placeorder)).click();
+}
+}
+
+
+
+
+
+
+
